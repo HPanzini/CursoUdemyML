82f8afebe8fc37ec213df1035c37fca6efe7f0b4
Hernan Panzini <hpanzini@gmail.com>
2020-07-18 22:11:59 -0300

    sin acentos

--- nlp-base-argentina.py
+++ nlp-base-argentina.py
@@ -16,7 +16,7 @@ import re
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 corpus = []
-for i in range(0,2875):
+for i in range(0,1705):
     review= re.sub('[^a-zA-Z]', ' ', dataset['Texto'][i])
     review=review.lower()
     review=review.split()
@@ -40,7 +40,7 @@ Y = dataset.iloc[:, -1].values
 # random-state=0 nos dice que no se va a mezclar
 
 from sklearn.model_selection import train_test_split
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.20, random_state=0)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.25, random_state=0)
 
 # entrenamiento (elegir modelo mas indicado evaluando accuracy vs test set)
 
@@ -62,3 +62,19 @@ print(cm)
 accuracy_score(Y_test,Y_pred)
 
 ## predecir documento
+
+urlpred = 'https://raw.githubusercontent.com/HPanzini/CursoUdemyML/master/Predecir.txt'
+datapred = pd.read_csv(urlpred, encoding='latin-1', sep='\t')
+print(new_review)
+corpuspred = []
+for i in range(0,5):
+    new_review = re.sub('[^a-zA-Z]', ' ', datapred['Texto'][i])
+    new_review = new_review.lower()
+    new_review = new_review.split()
+    ps = PorterStemmer()
+    new_review = [ps.stem(word) for word in new_review if not word in set(stopwords.words("spanish"))]
+    new_review = ' '.join(new_review)
+    new_corpus = [new_review]
+    new_X_test = cv.transform(new_corpus).toarray()
+    new_y_pred = classifier.predict(new_X_test)
+    print(new_y_pred)
